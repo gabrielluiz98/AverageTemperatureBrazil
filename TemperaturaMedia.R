40123ece61a964d414e55f1faf00ba94f6266d5c
Gabriel Luiz da Silva <gabrielluiz3011@gmail.com>
2021-08-15 13:39:49 -0300

    Instalando pacotes e carregando librarys

--- TemperaturaMedia.R
+++ TemperaturaMedia.R
@@ -1,1 +1,19 @@
 #Temperatura Média nas Cidades Brasileiras
+
+#Dataset
+#http://berkeleyearth.org/data
+
+#Instalando e carregando pacotes
+install.packages('readr')
+install.packages('data.table')
+install.packages('dplyr')
+install.packages('ggplot2')
+install.packages('scales')
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(scales)
+library(data.table)
+
+
+#Carregando o dataset
